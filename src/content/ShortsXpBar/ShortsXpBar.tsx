611e9ab9fb2c661ac5aec189da749f1b4448de5a
Olly Pethick <ollypethick@gmail.com>
2024-10-23 15:39:20 +0100

    Get daisyui working

--- src/content/ShortsXpBar/ShortsXpBar.tsx
+++ src/content/ShortsXpBar/ShortsXpBar.tsx
@@ -1,12 +1,26 @@
+import { Button } from "../../components/Button";
+
 export const ShortsXpBar = () => {
   return (
-    <div id="xp-bar" className="container" data-theme="dark">
-      <div className="h-44 top-5 w-full p-4 relative z-10">
+    <div data-theme="dark" className="bg-transparent">
+      <div
+        style={{
+          zIndex: 99999,
+        }}
+        className="w-80 p-4 top-14 relative flex justify-center"
+      >
         <progress
-          className="progress progress-success w-full"
+          style={{
+            height: "1.75rem",
+          }}
+          className="progress progress-success w-full h-7"
           value="10"
           max="100"
         ></progress>
+        <div>
+          <Button>Increase XP</Button>
+          <Button>Decrease XP</Button>
+        </div>
       </div>
     </div>
   );
